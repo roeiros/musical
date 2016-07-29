ee4910b5186606e42d5e339b25d19f6fa8ec8563
רועי רוזנטל <roei@domain.local>
2016-07-29 14:52:54 +0300

    validate email adress

--- public/myjs/myjs.js
+++ public/myjs/myjs.js
@@ -1,20 +1,30 @@
 var emailErr = document.getElementById('emailErr');
 function signup(e){
     e.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: '/user/signup',
-        data: {
-            name  : frmSignup.user_name.value,
-            email : frmSignup.user_email.value
-        },
-        success: function(){
-            alert('user added');
-        },
-        error : function(){
-            emailErr.style.display = 'block';
-        }
-    });
+    var emailVal = document.getElementById("mail").value;
+    var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if(pattern.test(emailVal)===true)
+    {
+        $.ajax({
+            type: "POST",
+            url: '/user/signup',
+            data: {
+                name  : frmSignup.user_name.value,
+                email : frmSignup.user_email.value
+            },
+            success: function(){
+                alert('user added');
+            },
+            error : function(){
+                emailErr.style.display = 'block';
+            }
+        });
+    }
+    else
+    {
+        $("#emailErr").text( emailVal + " is not a valid email");
+        emailErr.style.display = 'block';
+    }
 }
 
 $('#mail').keydown(function() {
